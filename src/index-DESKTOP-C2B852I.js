af00762a12be222854a7e726bfa376a8d11d4371
maugrim777 <gabriel.vlasceanu@outlook.com>
2020-05-13 12:48:08 +0300

    added Full Stack Certification from FCC

---
+++ src/index-DESKTOP-C2B852I.js
@@ -0,0 +1,36 @@
+import initTilt from './js/tilt';
+import initSr from './js/sr';
+import './style/main.scss';
+
+$('a[href^="#"]').on('click', function(event) {
+  var target = $(this.getAttribute('href'));
+  if (target.length) {
+    event.preventDefault();
+    $('html, body')
+      .stop()
+      .animate(
+        {
+          scrollTop: target.offset().top
+        },
+        1000
+      );
+  }
+});
+
+initSr();
+initTilt();
+
+let projectOrder = () => {
+  let projectChangeLogs = Array.from(document.getElementsByClassName('project-wrapper__change_log')) 
+
+  let projects=projectChangeLogs.map(log => log.parentElement.parentElement.parentElement)
+
+  let children=projectChangeLogs.map(log => log.parentElement.childElementCount)
+
+  for (let i=0; i<projects.length; i++) {
+    projects[i].style.order = 0 - children[i]
+  }
+
+
+}
+projectOrder()
